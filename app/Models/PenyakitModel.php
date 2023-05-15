7c2adeab2a30bb3787e91c0e2242ff7921dbfb57
krisanpaulino <36087959+krisanpaulino@users.noreply.github.com>
2023-05-15 21:19:30 +0700

    Update PenyakitModel.php

--- app/Models/PenyakitModel.php
+++ app/Models/PenyakitModel.php
@@ -68,7 +68,9 @@ class PenyakitModel extends Model
         $this->groupBy('penyakit.penyakit_kode');
         if ($jumlahgejala != null)
             $this->having('jumlahgejala >=', $jumlahgejala, false);
-        $this->orderBy('jumlahgejala', 'desc');
+        if ($ya != null || $tidak != null)
+            $this->orderBy('jumlahgejala', 'desc');
+        $this->orderBy('penyakit.penyakit_kode', 'asc');
         // dd($this->builder->getCompiledSelect());
         return $this->first();
     }
